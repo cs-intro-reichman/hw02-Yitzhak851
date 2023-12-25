81846812a3474fec1b5edbfc681486a2b97925bf
YBO <yitzhak851@gmail.com>
2023-12-25 23:06:00 +0200

    update damka mission

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,8 +1,14 @@
 /**
- *  Gets a command-line argument n (int), and prints an n-by-n damka board.
+ * Gets a command-line argument n (int), and prints an n-by-n damka board.
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int n = Integer.parseInt(args[0]);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
 	}
 }
--- Reverse.java
+++ Reverse.java
@@ -5,7 +5,7 @@ public class Reverse {
 			System.out.print(word.charAt(i));
 		}
 		System.out.println();
-		int middleIndex = word.length() / 2;	
+		int middleIndex = word.length() / 2;
 		System.out.println("The middle character is " + word.charAt(middleIndex));
 	}
 }
