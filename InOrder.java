19dde5da37e5338169861a0de5f0298e8e4807cc
YBO <yitzhak851@gmail.com>
2023-12-25 23:10:30 +0200

    Update InOrder.java

--- InOrder.java
+++ InOrder.java
@@ -1,9 +1,25 @@
 /**
- *  Generates and prints random integers in the range [0,10),
- *  as long as they form a non-decreasing sequence.
+ * Generates and prints random integers in the range [0,10),
+ * as long as they form a non-decreasing sequence.
  */
 public class InOrder {
-	public static void main (String[] args) {
-		//// Write your code here
+	public static void main(String[] args) {
+		int n, m;
+		
+		// % java InOrder
+		// 3 5
+		// % java InOrder
+		// 8
+		// % java InOrder
+		// 5 7 7 8
+		
+		// Tips: The first generated number always gets printed. 
+		// Consider using a do-while loop for the rest of the program. 
+		// See the lecture notes how to generate a random integer in
+		// the range [n , m), using Math.random.
+		// Play with (execute) this program many times, to get a feeling of the
+		// frequency of random lucky streaks 
+		// (scoring a consecutive number of shots in a basketball game, 
+		// investing consecutively in good stocks, and so on).
 	}
 }
