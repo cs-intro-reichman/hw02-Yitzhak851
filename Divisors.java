544909a389791fae7bc093adfff46fd0f94403cd
YBO <yitzhak851@gmail.com>
2023-12-25 22:10:03 +0200

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -1,7 +1,7 @@
 public class Divisors {
 	public static void main(String[] args) {
 		int x = Integer.parseInt(args[0]);
-		for (int i = 0; i < x; i++) {
+		for (int i = 1; i <= x; i++) {
 			if ((x % i) == 0) {
 				System.out.println(i);
 			}
