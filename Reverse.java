4aa686e3b3f9a43e14c8554043d95442f05eadd3
YBO <yitzhak851@gmail.com>
2023-12-26 16:08:02 +0200

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,11 +1,23 @@
 public class Reverse {
 	public static void main(String[] args) {
+		// Declrate variables
 		String word = args[0];
+		int word_length = word.length();
+		int middle_index = 0;
+		// continnoal
+		if (word_length == 0) {
+			System.out.println("");
+		} else if (word_length % 2 == 0) {
+			middle_index = (word_length / 2) ;
+		} else if (word_length % 2 != 0) {
+			middle_index = (word_length / 2) + 1;
+		}
+
 		for (int i = word.length() - 1; i >= 0; i--) {
 			System.out.print(word.charAt(i));
 		}
 		System.out.println();
-		int middleIndex = word.length() / 2;
-		System.out.println("The middle character is " + word.charAt(middleIndex));
+		
+		System.out.println("The middle character is " + word.charAt(middle_index));
 	}
 }
