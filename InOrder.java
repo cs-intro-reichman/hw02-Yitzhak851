a5ba149bf2a2fe69d66280ce23657d913414f125
YBO <yitzhak851@gmail.com>
2023-12-27 09:23:18 +0200

    update InOrder.java

--- InOrder.java
+++ InOrder.java
@@ -4,22 +4,18 @@
  */
 public class InOrder {
 	public static void main(String[] args) {
-		int n, m;
-		
-		// % java InOrder
-		// 3 5
-		// % java InOrder
-		// 8
-		// % java InOrder
-		// 5 7 7 8
-		
-		// Tips: The first generated number always gets printed. 
-		// Consider using a do-while loop for the rest of the program. 
-		// See the lecture notes how to generate a random integer in
-		// the range [n , m), using Math.random.
-		// Play with (execute) this program many times, to get a feeling of the
-		// frequency of random lucky streaks 
-		// (scoring a consecutive number of shots in a basketball game, 
-		// investing consecutively in good stocks, and so on).
+		int num1, num2, temp;
+		num1 = (int) (Math.random() * 10);
+		num2 = (int) (Math.random() * 10);
+		System.out.print(num1 + " ");
+
+		while (num1 <= num2) { 
+			System.out.print(num2 + " ");
+			if (num2 >= num1) {
+				num1 = num2;
+				temp = (int)(Math.random() * 10);
+				num2 = temp;
+			}
+		}
 	}
 }
