6e646ab61f44a3841bf371e2a02d7a9bd03b8b2e
YBO <yitzhak851@gmail.com>
2023-12-27 20:03:01 +0200

    Update OneOfEachStats.java

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -31,7 +31,7 @@ public class OneOfEachStats {
 			sum1 = (sum1 + count);
 		}
 		int numOfFamily = T;
-		System.out.println("Average: " + sum1 / numOfFamily + " children to get at least one of each gender.");
+		System.out.println("Average: " + (double)(sum1 / numOfFamily) + " children to get at least one of each gender.");
 		System.out.println("Number of families with 2 children: " + sum2);
 		System.out.println("Number of families with 3 children: " + sum3);
 		System.out.println("Number of families with 4 children: " + sum4);
