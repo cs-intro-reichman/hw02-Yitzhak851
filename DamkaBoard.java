7d81cb2978ba971c72055daa9c94cb02e8df82b7
YBO <yitzhak851@gmail.com>
2023-12-27 08:09:06 +0200

    Update DamkeBoard.java file

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,11 +5,14 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
 		for (int i = 0; i < n; i++) {
-			if (i%2 != 0) {
-				System.out.print(" ");
-			}
-			for (int j = 0; j < n; j++) {
-				System.out.print("* ");
+			if (i % 2 != 0) {
+				for (int j = 0; j < n; j++) {
+					System.out.print(" *");
+				}
+			} else {
+				for (int j = 0; j < n; j++) {
+					System.out.print("* ");
+				}
 			}
 			System.out.println();
 		}
