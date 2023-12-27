34449818bd3c41f015e6d580dbcdfe72718204c0
YBO <yitzhak851@gmail.com>
2023-12-27 20:01:17 +0200

    Update OneOfEachStats.java

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -1,8 +1,16 @@
 public class OneOfEachStats {
 	public static void main(String[] args) {
-		int T = Integer.parseInt(args[0]);
-		int sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0;
+		// declrate
+		int T, sum1, sum2, sum3, sum4;
+		// initional
+		T = Integer.parseInt(args[0]);
+		sum1 = 0;
+		sum2 = 0;
+		sum3 = 0;
+		sum4 = 0;
+		// run from 0 - T
 		for (int i = 0; i < T; i++) {
+			// declrate + initional
 			boolean itsAboy = false, itsAgirl = false;
 			int count = 0;
 			while ((itsAgirl && itsAboy) == false) {
@@ -22,7 +30,7 @@ public class OneOfEachStats {
 			}
 			sum1 = (sum1 + count);
 		}
-		double numOfFamily = T;
+		int numOfFamily = T;
 		System.out.println("Average: " + sum1 / numOfFamily + " children to get at least one of each gender.");
 		System.out.println("Number of families with 2 children: " + sum2);
 		System.out.println("Number of families with 3 children: " + sum3);
