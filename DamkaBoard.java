8b9cd3c24f28023c4237e05f7fecfaf80c55c97c
YBO <yitzhak851@gmail.com>
2023-12-28 20:52:28 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,6 +1,15 @@
+/**
+ * This class contain a "DamkaBoard" mission.
+ * 
+ * @author (Yitzhak baror)
+ * @version (28.12.2023)
+ */
 public class DamkaBoard {
 	public static void main(String[] args) {
+		// getting args from the user
 		int n = Integer.parseInt(args[0]);
+		// logic - run from:  0-> n
+		// if even - " " at the end, else, at the beggining
 		for (int i = 0; i < n; i++) {
 			if (i % 2 != 0) {
 				for (int j = 0; j < n; j++) {
