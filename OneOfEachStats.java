4fa98aa1d80743cfc9635d214e3dbcd612f7b1c3
YBO <yitzhak851@gmail.com>
2024-01-02 00:02:54 +0200

    Update OneOfEachStats.java

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -10,10 +10,12 @@ public class OneOfEachStats {
 	public static void main(String[] args) {
 		int T, seed, sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0;
 		double p;
-		//  = Math.random();
+		// get from the user T and seed
 		T = Integer.parseInt(args[0]);
 		seed = Integer.parseInt(args[1]);
+		// initional
 		Random generator = new Random(seed);
+		// run from 0 - T
 		for (int i = 1; i <= T; i++) {
 			boolean itsAboy = false, itsAgirl = false;
 			int count = 0;
@@ -28,6 +30,7 @@ public class OneOfEachStats {
 				}
 				count++;
 			}
+			// count how many families have 2,3,4 or more children
 			if (count == 2) {
 				sum2++;
 			} else if (count == 3) {
@@ -37,11 +40,13 @@ public class OneOfEachStats {
 			}
 			sum1 += count;
 		}
+		// print the results
 		double avg = (double) sum1 / (double) T;
 		System.out.println("Average: " + (avg) + " children to get at least one of each gender.");
 		System.out.println("Number of families with 2 children: " + sum2);
 		System.out.println("Number of families with 3 children: " + sum3);
 		System.out.println("Number of families with 4 or more children: " + sum4);
+		// print the most common number of children
 		if (sum2 > sum3) {
 			System.out.println("The most common number of children is 2.");
 		} else if (sum3 > sum4) {
